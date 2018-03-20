259de2e019beef7cefbc5d686026dbf207cff846
Stuart Kushner <stuartkushner@Stuarts-MacBook-Pro.local>
2018-03-20 11:05:19 -0400

    Made some changes

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,19 +1,21 @@
 import React from "react";
 
 const Results = props => (
-	<li className="list-group-item">
-		<h3>
-			<span>
-				<strong><a href={props.url} target="_blank">
-					{props.title}
-				</strong>
-			</span>
-			<span className="btn-group pull-right">
-				<button className="btn btn-success" onClick={() => props.saveArticle(props._id)}>Save Article</button>
-			</span>
-		</h3>
-		<p>Date Published: {props.date}
-	</li>
+	<div className="row">
+		<li className="list-group-item">
+			<h3>
+				<span>
+					<strong><a href={props.url} target="_blank">
+						{props.title}
+					</strong>
+				</span>
+				<span className="btn-group pull-right">
+					<button className="btn btn-success" onClick={() => props.saveArticle(props._id)}>Save Article</button>
+				</span>
+			</h3>
+			<p>Date Published: {props.date}
+		</li>
+	</div>
 );
 
 export default Results;
--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -1,21 +1,23 @@
 import React from "react";
 
 const Saved = props => (
-	<li className="list-group-item">
-		<h3>
-			<span>
-				<strong><a href={props.url} target="_blank">
-					{props.title}
-				</strong>
-			</span>
-			<span className="btn-group pull-right">
-				<button className="btn btn-danger" onClick={() => props.deleteSavedArticles(props._id)}>
-					Delete This Article
-				</button>
-			</span>
-		</h3>
-		<p>Date Published: {props.date}
-	</li>
+	<div className="row">
+		<li className="list-group-item">
+			<h3>
+				<span>
+					<strong><a href={props.url} target="_blank">
+						{props.title}
+					</strong>
+				</span>
+				<span className="btn-group pull-right">
+					<button className="btn btn-danger" onClick={() => props.deleteSavedArticles(props._id)}>
+						Delete This Article
+					</button>
+				</span>
+			</h3>
+			<p>Date Published: {props.date}
+		</li>
+	</div>
 );
 
 export default Saved;
